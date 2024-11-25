a9e7e0f94c07c27316e04f33a523577a0d37f471
Ashishmp <ar9034@srmist.edu.in>
2024-11-26 00:47:54 +0530

    upd

--- passwordGenerator/tailwind.config.js
+++ passwordGenerator/tailwind.config.js
@@ -1,9 +1,11 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: [],
+  content: [
+    "./src/**/*.{js,jsx,ts,tsx}", // Include all JS, JSX, TS, and TSX files in the src folder
+    "./index.html"         // Include the HTML file if necessary
+  ],
   theme: {
-    extend: {},
+    extend: {},                    // Use this to extend the default theme
   },
-  plugins: [],
+  plugins: [],                     // Add plugins here if needed
 }
-
