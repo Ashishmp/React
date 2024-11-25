7cf0d9e028d5d432ce51a0f4c4e6656be197fd67
Ashishmp <ar9034@srmist.edu.in>
2024-11-26 00:44:30 +0530

    React Projects

---
+++ passwordGenerator/src/App.jsx
@@ -0,0 +1,21 @@
+import { useState } from 'react'
+
+
+
+function App() {
+const [length, setLength] = useState(8)
+const [numberAllowed, setNumberAllowed] = useState(false)
+const [charactersAllowed, setCharacterAllowed] = useState(false)
+const [Password, setPassword]  = useState("")
+
+
+  const PasswordGenerator = () => {}
+
+  return (
+    <>
+      <h1 className='text-4xl text-center text-white'>Password Generator</h1>
+    </>
+  )
+}
+
+export default App
