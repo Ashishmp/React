38e96c838e4375b9112aa552385a87e16effe618
Ashish Ranjan <91370291+Ashishmp@users.noreply.github.com>
2024-11-28 11:47:48 +0000

    issue created

--- reduxtodo/src/components/Todos.jsx
+++ reduxtodo/src/components/Todos.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import { useSelector, useDispatch } from 'react-redux'
-import {removeTodo} from '../features/todo/todoSlice'
+import {removeTodo} from '../features/todo/TodoSlice'
 
 function Todos() {
     const todos = useSelector(state => state.todos)
