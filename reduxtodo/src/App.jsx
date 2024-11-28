1776a2641ad4527fba4acea93e16332e51f28c98
Ashish Ranjan <91370291+Ashishmp@users.noreply.github.com>
2024-11-28 11:22:38 +0000

    upda

--- reduxtodo/src/App.jsx
+++ reduxtodo/src/App.jsx
@@ -1,16 +1,16 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+
 import './App.css'
+import AddTodo from './components/Addtodo'
+import Todos from './components/Todos'
+
 
 function App() {
-  const [count, setCount] = useState(0)
-
+  
   return (
     <>
-      <div>
-        Learning Redux
-        </div>
+      <h1>Learn about redux toolkit</h1>S
+      <AddTodo />
+      <Todos />
     </>
   )
 }
--- reduxtodo/src/features/todo/TodoSlice.js
+++ reduxtodo/src/features/todo/TodoSlice.js
@@ -1,4 +1,4 @@
-import { createSlice, nanoid } from "@reduxjs/toolkit";
+import { createSlice, nanoid } from '@reduxjs/toolkit'
 
 const initialState = {
     todos: [{id:1, text: "Hello Learner"} ]
@@ -9,7 +9,20 @@ const todoSlice = createSlice({
     name:'todo',
    initialState,
    reducers:{
-    addTodo:  (state, action) => {},
-    removetodo: () => {}
+    addTodo:  (state, action) => {
+        const todo = {
+            id: nanoid(),
+            text: action.payload
+            
+        }
+        state.todos.push(todo)
+    },
+    removetodo: (state,action) => {
+        state.todos = state.todos.filter((todo) => todo.id !== action.payload)
+    }
    }
 })
+
+export const {addTodo, removetodo} = todoSlice.actions
+
+export default todoSlice.reducer
