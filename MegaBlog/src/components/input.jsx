814290cb6196da2187079a65f6058ca0fa0ef74c
Ashish Ranjan <91370291+Ashishmp@users.noreply.github.com>
2024-12-03 12:19:59 +0000

    changes

---
+++ MegaBlog/src/components/input.jsx
@@ -0,0 +1,29 @@
+import React, {useId} from "react";
+
+
+
+
+const Input = React.forwardRef(
+    function Input({
+        
+    }, ref ){
+        const id = useId()
+        return (
+        <div className=" w-full">
+            {
+                label && <label
+                className='inline-blick mb-1 pl-1'
+                htmlFor = {id}>
+                {label}
+                </label>
+            }
+            <input 
+            type = {type}
+            className={`px-3 py-2 rounded-lg bg-whi${className}`} 
+                />
+
+        </div>)
+    }
+)
+
+export default Input;
