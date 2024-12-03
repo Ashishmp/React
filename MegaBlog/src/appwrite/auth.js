887f3e252dcc32eac30dd8f7405ae33fac657a1a
Ashish Ranjan <91370291+Ashishmp@users.noreply.github.com>
2024-12-03 07:49:55 +0000

    auth service

--- MegaBlog/src/appwrite/auth.js
+++ MegaBlog/src/appwrite/auth.js
@@ -13,6 +13,46 @@ export class Authservice {
         .setEndpoint(conf.appwriteProjectId);
         this.account = new Account(this.client);
     }
+    async createAccount({email, password, name}) {
+        try {
+            const userAccount = await this.account.create(ID.unique(), email, password, name);
+            if(userAccount){
+               // return userAccount;
+                //call another method
+                this.login({email, password})
+
+            }
+            else{
+                return userAccount;
+            }
+        } catch (error) {
+            throw error;
+        }
+    } 
+
+    async login({user, password}) {
+        try {
+          return await this.account.createEmailSession(email, password)
+        } catch (error) {
+            throw error;
+        }
+    }
+async getCurrentUser() {
+    try {
+        await this.account.get()
+    } catch (error) {
+        throw error;// for custom error use console log
+    }
+
+    return null;
+}
+async logout(){
+    try {
+        await this.account.deleteSessions()
+    } catch (error) {
+        throw error;
+    }
+}
 }
 
 const authService = new Authservice();
