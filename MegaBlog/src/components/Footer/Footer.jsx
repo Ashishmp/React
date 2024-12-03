a0c807fffa4629598f558f24c7344626b04de9a8
Ashish Ranjan <91370291+Ashishmp@users.noreply.github.com>
2024-12-03 11:07:56 +0000

    appwrite backend completed

---
+++ MegaBlog/src/components/Footer/Footer.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+
+function Footer(){
+    return(
+        <div> Header</div>
+    )
+}
+
+
+export default Footer
--- MegaBlog/src/main.jsx
+++ MegaBlog/src/main.jsx
@@ -1,10 +1,15 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import store from './store/store.js'
+import { Provider } from 'react-redux'
 
-createRoot(document.getElementById('root')).render(
-  <StrictMode>
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <Provider store={store}>
     <App />
-  </StrictMode>,
+    </Provider>
+    
+  </React.StrictMode>,
 )
