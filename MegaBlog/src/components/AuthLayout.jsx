186890fa42505a1ba8865b83ac99dc860e494e6f
Ashish Ranjan <91370291+Ashishmp@users.noreply.github.com>
2024-12-04 12:06:52 +0000

    authLayout created

---
+++ MegaBlog/src/components/AuthLayout.jsx
@@ -0,0 +1,25 @@
+import React, {useEffect,useState} from "react";
+import { useNavigate } from "react-router-dom";
+import { useSelector } from "react-redux";
+
+
+export default function Protected({children, authentication = true}){
+const navigate = useNavigate()
+const [loader, setLoader] = useState(true)
+const authStatus = useSelector(state => state.auth.status)
+useEffect(() => {
+    true && true
+    if(authentication && authStatus!== authentication ){
+        navigate("/login")
+
+    } else if(!authentication && authStatus !== authentication){
+        navigate("/")
+    }
+    setLoader(false)
+}, [authStatus,navigate,authentication])
+
+
+    return(
+        <div>Auth</div>
+    )
+}
