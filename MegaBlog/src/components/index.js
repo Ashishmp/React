f07ba0d5258eeb3168438dc8b22f8ee2c42f2cdb
Ashish Ranjan <91370291+Ashishmp@users.noreply.github.com>
2024-12-05 07:21:53 +0000

    post page

--- MegaBlog/src/components/index.js
+++ MegaBlog/src/components/index.js
@@ -4,8 +4,11 @@ import Container from "./container/Container";
 import Logo from "./Logo";
 import LogoutBtn from "./Header/LogoutBtn";
 import RTE from "./RTE";
-
-
+import Signup from "./Signup";
+import Login from "./Login";
+import PostForm from "./post-form/PostForm";
+import Post from "./Post";
+import PostCard from "./PostCard"
 
 export{
     Header,
@@ -13,6 +16,11 @@ export{
     Container,
     Logo,
     LogoutBtn,
-    RTE
+    RTE,
+    Signup,
+    Login,
+    Post,
+    PostForm,
+    PostCard
 
 }
